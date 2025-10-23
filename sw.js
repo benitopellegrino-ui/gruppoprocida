7abc3ebbc6dca9264cd1551872305c94dc8f5819
benitopellegrino-ui <benitopellegrino@gmail.com>
2025-10-23 14:11:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,5 @@
-const CACHE_NAME = 'procida-cache-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './manifest.json',
-  './icon-512.png'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE_NAME = 'procida-cache-v2';
+const ASSETS = ['./','./index.html','./styles.css','./manifest.json','./icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
