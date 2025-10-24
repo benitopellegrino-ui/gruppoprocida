af66dc5066d82132cb4de7321a6cf5351bb430e3
benitopellegrino-ui <benitopellegrino@gmail.com>
2025-10-24 12:19:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE='gds-cache-v7';
-const ASSETS=['./','./index.html?v=7','./styles.css?v=7','./manifest.json','./icon-192.png','./icon-256.png','./icon-384.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png'];
+const CACHE='gds-cache-v8';
+const ASSETS=['./','./index.html?v=8','./styles.css?v=8','./manifest.json','./icon-192.png','./icon-256.png','./icon-384.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
  const req=e.request, acc=req.headers.get('accept')||''; const url=new URL(req.url);
  if(acc.includes('text/html')){
   e.respondWith(fetch(req).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(url.pathname+url.search,copy)); return res;})
-    .catch(()=>caches.match(url.pathname+url.search)||caches.match('./index.html?v=7'))); return;
+    .catch(()=>caches.match(url.pathname+url.search)||caches.match('./index.html?v=8'))); return;
  }
  e.respondWith(caches.match(url.pathname+url.search).then(c=>c||fetch(req).then(res=>{const copy=res.clone(); caches.open(CACHE).then(ca=>ca.put(url.pathname+url.search,copy)); return res;})));
 });
